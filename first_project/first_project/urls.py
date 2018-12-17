66de1cf1684fadd16250c36d862789be088ca007
Abhinav <abhinavjha98ald@gmail.com>
2018-12-17 15:53:12 +0530

    Added the project two

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path
+from django.conf.urls import include
 from hello_world import views
 
 urlpatterns = [
 	re_path('$',views.index,name = 'index'),
+	path('hello_world/',include('hello_world.urls')),
     path('admin/', admin.site.urls),
 ]
